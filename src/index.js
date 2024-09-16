8c8b89a009ca3bbd1ed5a614d471cc9f38bd5f50
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-09-16 20:28:00 +0530

    Finished add the final touches and implementing the navigation button functionalities.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@ import { displayLandingPage } from './landingPage.js';
 import { displayMenuPage } from "./menuPage.js";
 import { displayAboutPage } from "./aboutPage.js";
 
-displayAboutPage();
+function clearDOM () {
+    const contentContainer = document.querySelector("#content");
+    while (contentContainer.firstChild) contentContainer.removeChild(contentContainer.lastChild);
+}
+
+const homeButton = document.querySelector('#home');
+homeButton.addEventListener("click", () => {
+    clearDOM();
+    displayLandingPage();
+})
+
+const menuButton = document.querySelector('#menu');
+menuButton.addEventListener("click", () => {
+    clearDOM();
+    displayMenuPage();
+})
+
+const aboutButton = document.querySelector('#about');
+aboutButton.addEventListener("click", () => {
+    clearDOM();
+    displayAboutPage();
+})
+
+displayLandingPage();
 console.log("Hello, World!");
--- src/landingPage.js
+++ src/landingPage.js
@@ -5,18 +5,20 @@ import restaurantImage from "./images/restaurant-img.jpg"
 function displayLandingPage () {
 
     const mainContentElement = document.querySelector('#content');
-
+    const extraDiv = document.createElement('div');
     const headingElement = document.createElement('h1');
-    headingElement.textContent = 'No Brainer Cuisine Co.';
+    headingElement.textContent = 'Fine and Dine';
 
     const imgElement = document.createElement('img');
+    imgElement.className = 'mainImage';
     imgElement.src = restaurantImage;
     imgElement.alt = 'picture of a restaurant';
 
     const paragraphElement = document.createElement('p');
     paragraphElement.innerHTML = "One of the exceptional fine dine restaurant in the Bay Area. Dishes so good that it will take your taste buds for a tour around the world.<br> Menu so affordable that it doesn't burn a hole in your pocket. LOL!!!!";
 
-    mainContentElement.append(headingElement, imgElement, paragraphElement);
+    extraDiv.append(headingElement, imgElement, paragraphElement);
+    mainContentElement.appendChild(extraDiv);
 
 }
 
