4cefd5de2a5f60ea51b332f9f839b6c5b1b02633
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-09-16 17:19:14 +0530

    Finished setting up the homepage through the 'landingPage.js' file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import "./styles.css";
+import { headingElement, imgElement, paragraphElement } from './landingPage.js';
+
+const mainContentElement = document.querySelector('#content');
+mainContentElement.append(headingElement, imgElement, paragraphElement)
 console.log("Hello, World!");
---
+++ src/landingPage.js
@@ -0,0 +1,15 @@
+// Code to create the Homepage of the website
+
+import restaurantImage from "./images/restaurant-img.jpg"
+
+const headingElement = document.createElement('h1');
+headingElement.textContent = 'No Brainer Cuisine Co.';
+
+const imgElement = document.createElement('img');
+imgElement.src = restaurantImage;
+imgElement.alt = 'picture of a restaurant';
+
+const paragraphElement = document.createElement('p');
+paragraphElement.innerHTML = "One of the exceptional fine dine restaurant in the Bay Area. Dishes so good that it will take your taste buds for a tour around the world.<br> Menu so affordable that it doesn't burn a hole in your pocket. LOL!!!!";
+
+export {headingElement, imgElement, paragraphElement};
